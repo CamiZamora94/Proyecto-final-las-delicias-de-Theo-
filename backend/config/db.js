90c8d263b5ee01fef4f21fc5a26d4c462b66d19d
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-09 21:50:43 -0300

    Modificacion en la conexion de createConnection a createPool(para que exista mas de un camino de conexion)

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,13 @@ import mysql from "mysql2/promise"; //es una promesa es trabajar de forma asincr
 export let db; //es una variable que el db
 
 export async function connectDB() {
-  //en este archivo se conecta a la base de datos
-  db = await mysql.createConnection({
-    //crear la conexion por medio del await
+  db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
   });
 }
