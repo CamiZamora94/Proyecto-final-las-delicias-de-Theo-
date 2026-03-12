f7f55fe6cee12811220adf76a13bc57a611064e6
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-12 14:58:31 -0300

    Incorporacion de API de productos y recetas

--- frontend/src/services/productos.service.js
+++ frontend/src/services/productos.service.js
@@ -1,14 +1,21 @@
+import { getHeaders } from "./api";
+
 const API_URL = "http://localhost:5000/api/productos";
 
-export const getProductos = () => fetch(API_URL).then((res) => res.json());
+export const getProductos = () =>
+  fetch(API_URL, {
+    headers: getHeaders(),
+  }).then((res) => res.json());
 
 export const getProductoById = (id) =>
-  fetch(`${API_URL}/${id}`).then((res) => res.json());
+  fetch(`${API_URL}/${id}`, {
+    headers: getHeaders(),
+  }).then((res) => res.json());
 
 export const createProducto = (producto) =>
   fetch(API_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: getHeaders(),
     body: JSON.stringify(producto),
   }).then((res) => res.json());
 
@@ -16,20 +23,21 @@ export const createProducto = (producto) =>
 export const createBulkProductos = (lista) =>
   fetch(`${API_URL}/bulk`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: getHeaders(),
     body: JSON.stringify(lista),
   }).then((res) => res.json());
 
 export const updateProducto = (id, data) =>
   fetch(`${API_URL}/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers: getHeaders(),
     body: JSON.stringify(data),
   }).then((res) => res.json());
 
 export const eliminarProducto = async (id) => {
   const response = await fetch(`${API_URL}/${id}`, {
     method: "DELETE",
+    headers: getHeaders(),
   });
   return await response.json();
 };
