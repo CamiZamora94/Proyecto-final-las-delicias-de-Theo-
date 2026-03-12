0082f397dd03587f7a6ed62f8f53908510db4544
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-12 14:49:27 -0300

    No hay modificacion

---
+++ frontend/src/services/api.js
@@ -0,0 +1,10 @@
+export const getHeaders = () => {
+  const token = localStorage.getItem("token");
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+  return headers;
+};
