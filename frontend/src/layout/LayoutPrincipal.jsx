8ddfd9264e0861608e95fc1586a795995cc9edc9
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-12 14:48:55 -0300

    Eliminacion de panel principal en el text

--- frontend/src/layout/LayoutPrincipal.jsx
+++ frontend/src/layout/LayoutPrincipal.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar.jsx";
 
 
-export const LayoutPrincipal = () => { 
+export const LayoutPrincipal = () => {
     return (
         <div className="flex min-h-screen">
             <Sidebar />
             <div className="ml-64 p-6 w-full">
                 <header>
                     <div>
-                        <h2 className="text-2xl font-bold mb-4">Panel Principal</h2>
+                        <h2 className="text-2xl font-bold mb-4"></h2>
                     </div>
                     <div></div>
                 </header>
