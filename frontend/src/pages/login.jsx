729f3acfce58c742382eab1adc49ac373d54dac9
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-14 19:57:15 -0300

    Importacion de la imagen y del logo del empredimiento  para el login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Para redirigir tras el login
-import { login } from '../services/auth.service';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import pasteleria from '../assets/pasteleria.jpg';
+import logo from '../assets/logo.png';
+
 
 export const Login = () => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
   const [error, setError] = useState(null);
+  const { login } = useAuth();
   const navigate = useNavigate();
 
-  // Actualiza el estado cuando el usuario escribe
   const handleChange = (e) => {
     setCredentials({
       ...credentials,
@@ -19,48 +22,45 @@ export const Login = () => {
     e.preventDefault();
     setError(null);
 
-    try {
-      const data = await login(credentials);
-      
-      if (data.token) {
-        // Si el login es exitoso, redirigimos al Resumen o Inventario
-        navigate('/resumen'); 
-      } else {
-        setError(data.message || "Error al iniciar sesión");
-      }
-    } catch (err) {
-      setError("No se pudo conectar con el servidor.");
+    const result = await login(credentials.email, credentials.password);
+
+    if (result.success) {
+      navigate('/');
+    } else {
+      setError(result.message || "Error al iniciar sesión");
     }
   };
 
   return (
     <div className="login-container">
-      <h2>Panel de Gestión - Pastelería</h2>
+      
+      <img src={logo} alt="Logo" />
+      <h2>Inicio de sesión - Pastelería "Las delicias de Theo"</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Email:</label>
-          <input 
-            type="email" 
-            name="email" 
-            value={credentials.email} 
-            onChange={handleChange} 
-            required 
+          <label>Correo electrónico:</label>
+          <input
+            type="email"
+            name="email"
+            value={credentials.email}
+            onChange={handleChange}
+            required
           />
         </div>
         <div>
           <label>Contraseña:</label>
-          <input 
-            type="password" 
-            name="password" 
-            value={credentials.password} 
-            onChange={handleChange} 
-            required 
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+            required
           />
         </div>
-        
+
         {error && <p style={{ color: 'red' }}>{error}</p>}
-        
-        <button type="submit">Ingresar</button>
+
+        <button type="submit">Iniciar sesión</button>
       </form>
     </div>
   );
