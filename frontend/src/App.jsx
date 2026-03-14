5730a1be3e2fb4ccd72ab2192dbaf1ef9e4d0893
Camila Gisel Zamora <camizamora_89@hotmail.com>
2026-03-14 19:56:19 -0300

    Se agrego la importacion de privado y de authProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LayoutPrincipal } from './layout/LayoutPrincipal.jsx';
 import { Resumen } from './pages/Resumen.jsx';
-import { Inventario } from "./pages/inventario.jsx"; 
+import { Inventario } from "./pages/inventario.jsx";
 import { Ventas } from './pages/Ventas.jsx';
-import { Login } from './pages/login.jsx'; 
-import Sidebar from "./components/Sidebar.jsx";
+import { Login } from './pages/login.jsx';
+import { Privado } from './utils/Privado.jsx';
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
-function AppWithSidebar() {
+function App() {
   return (
-    <BrowserRouter>
-      <div style={{ display: 'flex', height: '100vh' }}>
-        <Sidebar />
-        <div style={{ flex: 1, padding: '20px' }}>
-          <Routes>
-            <Route path="/ingreso" element={<Login />} />
-            <Route path="/" element={<LayoutPrincipal />}>
-              <Route index element={<Resumen />} />
-              <Route path="ventas" element={<Ventas />} />
-              <Route path="inventario" element={<Inventario />} />
-              <Route path="recetario" element={<h1>Página de Recetario</h1>} />
-            </Route>
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/ingreso" element={<Login />} />
+          <Route path="/" element={<Privado><LayoutPrincipal /></Privado>}>
+            <Route index element={<Resumen />} />
+            <Route path="ventas" element={<Ventas />} />
+            <Route path="inventario" element={<Inventario />} />
+            <Route path="recetario" element={<h1>Página de Recetario</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
-export default AppWithSidebar;
+export default App;
